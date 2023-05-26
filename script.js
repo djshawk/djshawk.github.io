36177bf380a146735190c35e13ad243797469f94
djshawk <80673031+djshawk@users.noreply.github.com>
2023-05-27 01:39:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,24 @@ function showButtons() {
 function showMessage(response) {
   var messageContainer = document.getElementById("messageContainer");
   var message = document.getElementById("message");
-  message.textContent = response;
+
+  if (response === "Yes") {
+    // Display flower bouquet emojis
+    message.innerHTML = "🌸🌷🌺";
+    // Simulate sending an email (replace with your own implementation)
+    simulateEmailSending();
+  } else if (response === "No") {
+    message.innerHTML = "Sorry, that's not an option.";
+  }
+
   messageContainer.classList.remove("hidden");
+}
+
+// Simulate email sending process
+function simulateEmailSending() {
+  // Replace with your own implementation to send an email
+  // This is a placeholder to show a console message
+  console.log("Email sent!");
 }
 
 // Event listener to show buttons after the video finishes playing
